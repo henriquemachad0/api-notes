e0b38bc96fe30df0cd6526889cdc0ad440dad50d
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-03-07 14:12:15 -0300

    feat: add userId in requests main routes

--- src/main/routes/note-routes.routes.ts
+++ src/main/routes/note-routes.routes.ts
@@ -6,13 +6,14 @@ import { makeGetAllNoteController } from '../factories/controllers/note/get-all-
 import { makeGetByIdNoteController } from '../factories/controllers/note/get-by-id-note-factory'
 import { makeRemoveNoteController } from '../factories/controllers/note/remove-note-factory'
 import { makeUpdateNoteController } from '../factories/controllers/note/update-note-controller-factory'
+import { ensureAuthenticated } from '../factories/middlewares/ensure-authenticated'
 
 const router = Router()
 
-router.post('/create', adaptRoute(makeCreateNoteController()))
-router.get('/', adaptRoute(makeGetAllNoteController()))
-router.get('/:_id', adaptRoute(makeGetByIdNoteController()))
-router.patch('/update/:_id', adaptRoute(makeUpdateNoteController()))
-router.delete('/remove/:_id', adaptRoute(makeRemoveNoteController()))
+router.post('/create', ensureAuthenticated, adaptRoute(makeCreateNoteController()))
+router.get('/', ensureAuthenticated, adaptRoute(makeGetAllNoteController()))
+router.get('/:_id', ensureAuthenticated, adaptRoute(makeGetByIdNoteController()))
+router.patch('/update/:_id', ensureAuthenticated, adaptRoute(makeUpdateNoteController()))
+router.delete('/remove/:_id', ensureAuthenticated, adaptRoute(makeRemoveNoteController()))
 
 export default router
