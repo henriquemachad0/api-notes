3b9ce3bc6141e3003ee442fec59420d9cb7a3ffd
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-07-11 12:09:08 -0300

    fix: commit

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -2,8 +2,6 @@ import { Controller } from '@/presentation/protocols'
 
 import { Request, Response } from 'express'
 
-import fs from 'fs'
-
 export const adaptRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
     const request = {
@@ -16,15 +14,7 @@ export const adaptRoute = (controller: Controller) => {
 
     const httpResponse = await controller.handle(request)
 
-    if (httpResponse.body?.filename) {
-      res.download(`public/files/${httpResponse.body.filename}`, function () {
-        fs.unlink(`public/files/${httpResponse.body.filename}`, function (err) {
-          if (err) {
-            console.log(err)
-          }
-        })
-      })
-    } else if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
+    if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
       res.status(httpResponse.statusCode).json(httpResponse.body)
     } else {
       res.status(httpResponse.statusCode).json({
